9c4efff5718f354877f77686fc856d24636e5851
j.hwp <we1905@icloud.com>
2024-05-10 22:32:17 +0900

    v0.4 plus option6

--- 2024-OSS-Project1.sh
+++ 2024-OSS-Project1.sh
@@ -55,8 +55,41 @@ do
         cat "$3" | sort -t ',' -n -r -k 2 | head -3 | awk -F, '{printf("%s vs %s (%s)\n%d %s\n\n", $3, $4, $1, $2, $7)}'
       fi
       ;;
+    4)
+      for i in $ (seq 1 20)
+      do
+        team=$(cat "$1" | awk -v rank=$i -F, '$6==rank {print $1}')
+        echo "$i $team"
+        pg=$(cat "$2" | awk -v a=$team -F, '$4==a {print $1, $7}' | sort -r -k 2)
+      done  
+      ;;
+    
+    5) 
+      echo - n "Do you want to modify the format of date? (y/n) : "
+      read res
 
+      ;; 
+    
+    6) 
+      echo "1) Arsenal                  11) Liverpool"
+      echo "2) Tottenham Hotspur        12) Chelsea"
+      echo "3) Machester City           13) West Ham United"
+      echo "4) Leicester City           14) Watford"
+      echo "5) Crystal Palace           15) Newcastle United"
+      echo "6) Everton                  16) Cardiff City"
+      echo "7) Burnley                  17) Fulham"
+      echo "8) Southhampton             18) Brighton & Hove Albion"
+      echo "9) AFC Bournemouth          19) Huddersfield Town"
+      echo "10) Machester United        20) Wolverhampton Wanderers"
+      echo -n "Enter your team number : "
+      read n 
+      echo 
 
+      team=$(cat "$1" | awk -v i=$n -F, 'NR==i+1 {print $1}')
+      max=$(cat "$3" | awk -v t=$team -F, '$3==t {print $5 - $6}' | sort -n -r | head -1)
+      cat "$3" | awk -v t=$team -v m=$max -F, '$3==t && $5-$6==m {printf("%s\n%s %d vs %d %s\n\n", $1, $3, $5, $6, $4)}'
+
+      ;;
 
     7) 
     echo "Bye!"
